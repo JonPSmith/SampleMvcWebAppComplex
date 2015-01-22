812af433ff01fec1b0f4f7c76005fc32fe1b3d3f
JonSmith <jon.smith@selectiveanalytics.com>
2015-01-22 11:58:37 +0000

    Added pre-build views to speed up EF startup

--- DataLayer/App_Start/EfConfiguration.cs
+++ DataLayer/App_Start/EfConfiguration.cs
@@ -8,8 +8,10 @@ namespace DataLayer.App_Start
 {
     public class EfConfiguration : DbConfiguration
     {
-        //The configuration below is designed to speed up the initial startup of EF
-        //See http://romiller.com/2014/06/10/reducing-code-first-database-chatter/
+        //NOTES ON SPEEDING UP EF STARTUP
+        //I have done two things.
+        //1. Pre generated views see https://msdn.microsoft.com/en-us/data/dn469601.aspx
+        //2. Added configuration below is designed to speed up the initial startup of EF. See http://romiller.com/2014/06/10/reducing-code-first-database-chatter/
         public EfConfiguration()
         {
             SetDatabaseInitializer<AdventureWorksLt2012>(new NullDatabaseInitializer<AdventureWorksLt2012>());
