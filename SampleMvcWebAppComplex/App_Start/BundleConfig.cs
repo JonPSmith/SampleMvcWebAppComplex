5f90fe21e39ee44b59ebf707aca3edc8120f2541
JonSmith <jon.smith@selectiveanalytics.com>
2015-02-13 09:20:52 +0000

    Enabled bundling of JavaScript/CSS

--- SampleMvcWebAppComplex/App_Start/BundleConfig.cs
+++ SampleMvcWebAppComplex/App_Start/BundleConfig.cs
@@ -43,7 +43,7 @@ namespace SampleMvcWebAppComplex
 
             // Set EnableOptimizations to false for debugging. For more information,
             // visit http://go.microsoft.com/fwlink/?LinkId=301862
-            BundleTable.EnableOptimizations = false;
+            //BundleTable.EnableOptimizations = false;
         }
     }
 }
